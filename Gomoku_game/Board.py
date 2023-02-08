cd709865e6fdb6daa436ad3ca0c5dcf085738ab0
unknown <palfi.gellert.akos@t-systems.hu>
2023-02-08 17:00:38 +0100

    Only allowing pieces in right positions, Player class implementation

--- Gomoku_game/Board.py
+++ Gomoku_game/Board.py
@@ -10,6 +10,10 @@ class Board:
     def create_board(self, board_size):
         board = np.zeros((board_size, board_size))
         return board
-    
-    def check_win(self, location):
-        pass
+
+    def in_bounds(self, y, x):
+        return y >= 0 and y < BOARD_SIZE and x >= 0 and x < BOARD_SIZE
+
+    def is_empty(self, row, col):
+        return self.board[row][col] == 0
+
--- Gomoku_game/Gomoku.py
+++ Gomoku_game/Gomoku.py
@@ -1,6 +1,7 @@
 import itertools
 
 import pygame
+from Board import Board
 
 ROWS = 19
 COLS = 19
@@ -21,6 +22,8 @@ class Gomoku:
         self.rows = ROWS
         self.width = COLS * size
         self.height = ROWS * size
+        self.board = Board()
+        self.last_player_move = "BLACK"
         self.screen_size_vertical = self.height + PADDING
         self.screen_size_horizontal = self.width + PADDING
         self.piece_size = piece_size
@@ -47,8 +50,23 @@ class Gomoku:
         for start, end in lines:
             pygame.draw.line(self.screen, Colors.BLACK, start, end, width=2)
 
-    def drop_piece(self, position):
-        pygame.draw.circle(self.screen, Colors.BLACK, position, self.piece_size)
+    def draw_piece(self, x, y):
+        half = self.size // 2
+        # by adding half to the x,y  coordinates we ensure that they
+        # "snap" into position, so you can click anywhere in a cell
+        # and it places the piece on the closest edge
+        # -1 is used because of array indexing starting at 0
+        x = (x + half) // self.size - 1
+        y = (y + half) // self.size - 1
+
+        if self.board.in_bounds(x, y) and self.board.is_empty(x, y):
+            # switch x and y, because 2D array indexing takes in the row (y) coordinate first
+            # then the column (x)
+            self.board.drop_piece(y, x)
+            x = x * self.size + PADDING
+            y = y * self.size + PADDING
+
+            pygame.draw.circle(self.screen, Colors.BLACK, (x, y), self.piece_size)
 
     def draw_board(self):
         self.draw_background()
@@ -57,12 +75,14 @@ class Gomoku:
     def play(self):
         running = True
         self.draw_board()
+        pygame.display.set_caption("Gomoku (Connet 5)")
+
         while running:
             # Did the user click the window close button?
             for event in pygame.event.get():
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     pos = pygame.mouse.get_pos()
-                    self.drop_piece(pos)
+                    self.draw_piece(*pos)
                 if event.type == pygame.QUIT:
                     running = False
 
